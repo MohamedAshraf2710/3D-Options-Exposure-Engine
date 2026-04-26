ff4e3fb34a5550a7df1f6547a6c673c3cc308123
MohamedAshraf2710 <medo71505@gmail.com>
2026-04-26 15:14:38 +0300

    fix: implement rate limiting and session handling to resolve YFRateLimitError

--- data_engine.py
+++ data_engine.py
@@ -1,28 +1,36 @@
 import yfinance as yf
 import pandas as pd
 import numpy as np
+import requests_cache
+from requests import Session
+from requests_ratelimiter import LimiterSession
 
 def get_real_options_data(ticker_symbol):
-    ticker = yf.Ticker(ticker_symbol)
+  
+    session = LimiterSession(per_second=2) 
+    ticker = yf.Ticker(ticker_symbol, session=session)
     
-    expiries = ticker.options
-    if not expiries:
-        return None, None, None
-    
-    all_calls = []
-    current_price = ticker.history(period="1d")['Close'].iloc[-1]
-    
-
-    for date in expiries[:5]:
-        opt = ticker.option_chain(date)
-        calls = opt.calls
-        calls['expiry'] = date
+    try:
+        expiries = ticker.options
+        if not expiries:
+            return None, None, None
         
-        calls['days_to_expiry'] = (pd.to_datetime(date) - pd.Timestamp.today()).days
-        all_calls.append(calls)
-    
-    df = pd.concat(all_calls)
-    
-    df = df[(df['strike'] > current_price * 0.8) & (df['strike'] < current_price * 1.2)]
-    
-    return df, current_price, expiries[:5]
+        history = ticker.history(period="1d")
+        if history.empty:
+            return None, None, None
+        S = history['Close'].iloc[-1]
+        
+        all_calls = []
+      
+        for date in expiries[:3]:
+            opt = ticker.option_chain(date)
+            calls = opt.calls
+            calls['expiry'] = date
+            calls['days_to_expiry'] = (pd.to_datetime(date) - pd.Timestamp.today()).days
+            all_calls.append(calls)
+        
+        df = pd.concat(all_calls)
+        df = df[(df['strike'] > S * 0.9) & (df['strike'] < S * 1.1)] 
+        return df, S, expiries[:3]
+    except Exception as e:
+        return None, None, None
